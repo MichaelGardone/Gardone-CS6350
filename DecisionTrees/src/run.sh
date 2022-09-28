af75e213314181cbcc1971a027d17dfad8c4247d
M Gardone <30506335+MichaelGardone@users.noreply.github.com>
2022-09-28 15:12:44 -0600

    Adding run.sh
    Adding run.sh to DecisionTrees/src to make running automatic

---
+++ DecisionTrees/src/run.sh
@@ -0,0 +1,14 @@
+
+printf "Running car dataset..."
+
+python3 car.py
+
+printf "Finished car dataset!\n\n\n\n\n"
+
+printf "Running bank dataset..."
+
+python3 bank.py
+
+printf "Finished bank dataset!\n"
+
+printf "All tests finished!"
