096c45baf81daff7e99d0fd35c9635af94eac54b
MichaelGardone <michaelgardone@gmail.com>
2022-10-20 18:34:05 -0600

    updating run.sh in both EnsembleLearning and LinearRegression to actually be executable + making them more open to being called from the root of the project

--- EnsembleLearning/run.sh
+++ EnsembleLearning/run.sh
@@ -1,36 +1,73 @@
+#!/bin/bash
 printf "Running tests for EnsembleLearning...\n"
 
-printf "Running tests for AdaBoost... (in output/output_boost.txt)\n"
+if [[ -d "EnsembleLearning" ]]
+then
+    printf "Running tests for AdaBoost... (in output/output_boost.txt)\n"
 
-python3 boost.py > output/output_boost.txt
+    python3 EnsembleLearning/boost.py > EnsembleLearning/output/output_boost.txt
 
-printf "Finished tests for AdaBosst!\n\n"
+    printf "Finished tests for AdaBosst!\n\n"
 
-printf "Running tests for Bagging... (in output/output_bagging.txt)\n"
+    printf "Running tests for Bagging... (in output/output_bagging.txt)\n"
 
-python3 bagging.py > output/output_bagging.txt
+    python3 EnsembleLearning/bagging.py > EnsembleLearning/output/output_bagging.txt
 
-printf "Finished tests for Bagging!\n\n"
+    printf "Finished tests for Bagging!\n\n"
 
-printf "Running tests for Random Forest... (in output/output_forest.txt)\n"
+    printf "Running tests for Random Forest... (in output/output_forest.txt)\n"
 
-python3 forest.py > output/output_forest.txt
+    python3 EnsembleLearning/forest.py > EnsembleLearning/output/output_forest.txt
 
-printf "Finished tests for Random Forest!\n\n"
+    printf "Finished tests for Random Forest!\n\n"
 
-printf "==== These next tests are Q2 C and D, you can jump out now! ===\n"
-printf "These tests produce output to inform you of what step you're on, so these aren't piped.\n"
+    printf "==== These next tests are Q2 C and D, you can jump out now! ===\n"
+    printf "These tests produce output to inform you of what step you're on, so these aren't piped.\n"
 
-printf "Running tests for Q2, part C...\n"
+    printf "Running tests for Q2, part C...\n"
 
-python3 q2_c.py
+    python3 EnsembleLearning/q2_c.py
 
-printf "Finished tests for Q2, part C!\n\n"
+    printf "Finished tests for Q2, part C!\n\n"
 
-printf "Running tests for Q2, part E...\n"
+    printf "Running tests for Q2, part E...\n"
 
-python3 q2_c.py
+    python3 EnsembleLearning/q2_c.py
 
-printf "Finished tests for Q2, part E!\n\n"
+    printf "Finished tests for Q2, part E!\n\n"
+else
+    printf "Running tests for AdaBoost... (in output/output_boost.txt)\n"
+
+    python3 boost.py > output/output_boost.txt
+
+    printf "Finished tests for AdaBosst!\n\n"
+
+    printf "Running tests for Bagging... (in output/output_bagging.txt)\n"
+
+    python3 bagging.py > output/output_bagging.txt
+
+    printf "Finished tests for Bagging!\n\n"
+
+    printf "Running tests for Random Forest... (in output/output_forest.txt)\n"
+
+    python3 forest.py > output/output_forest.txt
+
+    printf "Finished tests for Random Forest!\n\n"
+
+    printf "==== These next tests are Q2 C and D, you can jump out now! ===\n"
+    printf "These tests produce output to inform you of what step you're on, so these aren't piped.\n"
+
+    printf "Running tests for Q2, part C...\n"
+
+    python3 q2_c.py
+
+    printf "Finished tests for Q2, part C!\n\n"
+
+    printf "Running tests for Q2, part E...\n"
+
+    python3 q2_c.py
+
+    printf "Finished tests for Q2, part E!\n\n"
+fi
 
 printf "All tests finished!\n"
--- LinearRegression/run.sh
+++ LinearRegression/run.sh
@@ -1,21 +1,48 @@
-printf "Running tests for LinearRegression...\n"
+#!/bin/bash
 
-printf "Running tests for Batch GD...\n"
+if [[ -d "LinearRegression" ]]
+then
+    printf "Running tests for LinearRegression...\n"
 
-python3 batch.py
+    printf "Running tests for Batch GD...\n"
 
-printf "Finished tests for Batch GD!\n\n"
+    python3 LinearRegression/batch.py
 
-printf "Running tests for Stochastic GD...\n"
+    printf "Finished tests for Batch GD!\n\n"
 
-python3 stochastic.py
+    printf "Running tests for Stochastic GD...\n"
 
-printf "Finished tests for Stochastic GD!\n\n"
+    python3 LinearRegression/stochastic.py
 
-printf "Running tests for analytical equation...\n"
+    printf "Finished tests for Stochastic GD!\n\n"
 
-python3 analytical.py
+    printf "Running tests for analytical equation...\n"
 
-printf "Finished tests for analytical equation!\n\n"
+    python3 LinearRegression/analytical.py
 
-printf "All tests finished!\n"
+    printf "Finished tests for analytical equation!\n\n"
+
+    printf "All tests finished!\n"
+else
+    printf "Running tests for LinearRegression...\n"
+
+    printf "Running tests for Batch GD...\n"
+
+    python3 batch.py
+
+    printf "Finished tests for Batch GD!\n\n"
+
+    printf "Running tests for Stochastic GD...\n"
+
+    python3 stochastic.py
+
+    printf "Finished tests for Stochastic GD!\n\n"
+
+    printf "Running tests for analytical equation...\n"
+
+    python3 analytical.py
+
+    printf "Finished tests for analytical equation!\n\n"
+
+    printf "All tests finished!\n"
+fi
